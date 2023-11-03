37d6beda5d83df0e763eb4cb16e5861e4d747ef3
oliveiraFreddie <freddie.oliveira95@gmail.com>
2023-11-03 17:58:51 +0000

    public

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const webpack = require('webpack')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/principal.js',
+  output: {
+    filename: 'principal.js',
+    path: __dirname + '/public'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader', // Adiciona CSS a DOM injetando a tag <style>
+          'css-loader' // Interpreta @import, url()...
+        ]
+      }
+    ]
+  }
+}
