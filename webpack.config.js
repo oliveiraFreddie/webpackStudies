521bcc0e81c1315cdf1c8e1d9a657aad8275f3fa
oliveiraFreddie <freddie.oliveira95@gmail.com>
2023-11-04 10:49:20 +0000

    optimized to build

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,39 @@
+const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
-  mode: 'development',
+  mode: modoDev ? 'development' : 'production',
   entry: './src/principal.js',
   output: {
     filename: 'principal.js',
     path: __dirname + '/public'
   },
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin({
+        cache: true,
+        parallel: true
+      })
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'estilo.css'
+    })
+  ],
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.s?[ac]ss$/,
         use: [
-          'style-loader', // Adiciona CSS a DOM injetando a tag <style>
-          'css-loader' // Interpreta @import, url()...
+          MiniCssExtractPlugin.loader, //Esse conflita com o debaixo
+          //'style-loader', // Adiciona CSS a DOM injetando a tag <style>
+          'css-loader', // interpreta @import, url()
+          'sass-loader'
         ]
       }
     ]
